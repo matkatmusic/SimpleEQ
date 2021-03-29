4ed2b9fd6740ef96b084190fd28a3384a3ea1aeb
Charles Schiermeyer <matkatmusic@gmail.com>
2021-03-29 16:30:28 -0700

    added frequency labels
    thinned out number of frequencies

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -315,9 +315,9 @@ void ResponseCurveComponent::resized()
     
     Array<float> freqs
     {
-        20, 30, 40, 50, 100,
-        200, 300, 400, 500, 1000,
-        2000, 3000, 4000, 5000, 10000,
+        20, /*30, 40,*/ 50, 100,
+        200, /*300, 400,*/ 500, 1000,
+        2000, /*3000, 4000,*/ 5000, 10000,
         20000
     };
     
@@ -359,6 +359,38 @@ void ResponseCurveComponent::resized()
     }
     
 //    g.drawRect(getAnalysisArea());
+    
+    g.setColour(Colours::lightgrey);
+    const int fontHeight = 10;
+    g.setFont(fontHeight);
+    
+    for( int i = 0; i < freqs.size(); ++i )
+    {
+        auto f = freqs[i];
+        auto x = xs[i];
+        
+        bool addK = false;
+        String str;
+        if( f > 999.f )
+        {
+            addK = true;
+            f /= 1000.f;
+        }
+        
+        str << f;
+        if( addK )
+            str << "k";
+        str << "Hz";
+        
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+        
+        Rectangle<int> r;
+        r.setSize(textWidth, fontHeight);
+        r.setCentre(x, 0);
+        r.setY(1);
+        
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+    }
 }
 
 juce::Rectangle<int> ResponseCurveComponent::getRenderArea()
